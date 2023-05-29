1a86d5af4971cac660ada75328bd407673c90149
J.T. Buice <jbuice@kainostech.com>
2023-05-29 13:10:35 -0500

    Update vote.sh

--- vote.sh
+++ vote.sh
@@ -6,7 +6,7 @@ echo -n "Please enter your vote (yes or no): "
 read options
 
 while IFS= read -r account_name; do
-    echo "Processing account: ${account_name}"
+    echo "Voting account: ${account_name}"
     cleos -u https://mainnet.telos.net push transaction "{
         \"delay_sec\": 0,
         \"max_cpu_usage_ms\": 0,
